7bf00c58165d72b8abaf2a395944941f88dae71b
Melissa A <41melissa.allen@gmail.com>
2018-07-10 19:47:01 -0400

    udpate

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@ $("#find-comic").on("click", function (event) {
 
 	event.preventDefault();
 	var comic = $("#searchTerm").val().trim();
-    var queryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&callbackname=callback&appid=BrandenH-MarvelSi-PRD-6668815a4-427e2463&siteid=0&version=967&QueryKeywords=" + comic + "%20comic&AvailableItemsOnly=true&MaxEntries=10";
+	var queryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&callbackname=callback&appid=BrandenH-MarvelSi-PRD-6668815a4-427e2463&siteid=0&version=967&QueryKeywords=" + comic + "%20comic&AvailableItemsOnly=true&MaxEntries=10";
 
 	$.ajax({
 		url: queryURL,
@@ -14,6 +14,10 @@ $("#find-comic").on("click", function (event) {
 	})
 		.then(function (callback) {
 
+			var hotmess = $("<p>").text(callback.Timestamp);
+			console.log(callback.Product[0]);
+			$("#hotmessdisplay").prepend(hotmess);
+			console.log(callback);
 		});
-	console.log(queryURL);
 });
+
