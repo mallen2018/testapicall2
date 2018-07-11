802aba317250438e4681982f5f4543fd7c3288ed
Melissa A <41melissa.allen@gmail.com>
2018-07-10 20:02:38 -0400

    update call

--- javascript/app.js
+++ javascript/app.js
@@ -4,13 +4,18 @@ $("#find-comic").on("click", function (event) {
 	var comic = $("#searchTerm").val().trim();
 	var queryURL = "http://open.api.ebay.com/shopping?callname=FindProducts&responseencoding=JSON&callbackname=callback&appid=BrandenH-MarvelSi-PRD-6668815a4-427e2463&siteid=0&version=967&QueryKeywords=" + comic + "%20comic&AvailableItemsOnly=true&MaxEntries=10";
 
+	$.ajaxPrefilter( function (options) {
+		if (options.crossDomain && jQuery.support.cors) {
+		  var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+		  options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+		  //options.url = "http://cors.corsproxy.io/url=" + options.url;
+		}
+	   });
+
 	$.ajax({
 		url: queryURL,
 		method: "GET",
-		dataType: "jsonp",
-		crossDomain: true,
-		jsonp: "false",
-		jsonpCallback: "callback"
+		
 	})
 		.then(function (callback) {
 
